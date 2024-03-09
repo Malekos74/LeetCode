4111463595f3c2b98acb5a349e3891cab99890ea
Malek Miled <ge48bet@mytum.de>
2024-03-09 17:18:27 +0100

    [FEAT] Implemented lengthOfLongestSubstringBF

--- longest_substring_without_repeating_characters.py
+++ longest_substring_without_repeating_characters.py
@@ -28,31 +28,50 @@ s consists of English letters, digits, symbols and spaces.
 
 '''
 
-def lengthOfLongestSubstring(s):
-        """
+# The brute force way
+def lengthOfLongestSubstringBF(s):
+    """
         :type s: str
         :rtype: int
-        """
+    """
+    max_len = 0
+    tmp = 0
+    reset = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+              "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+              "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " "]
     
+    usable = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+              "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+              "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " "]
     
+    for x in s:
+        try:
+            usable.remove(x)
+            tmp += 1
+            if tmp > max_len: max_len = tmp
 
+        except ValueError:
+            tmp = 0
+            usable = reset
+            
+    return max_len
 # Some Test cases
 if __name__ == "__main__":
     s = "abcabcbb"
     print("Example 1:")
     print("Input:", s)
-    print("Output:", lengthOfLongestSubstring(s))
-    print("Expected: 3")
+    print("Output:", lengthOfLongestSubstringBF(s))
+    print("Expected: 3\n")
     
     s = "bbbbb"
     print("Example 2:")
     print("Input:", s)
-    print("Output:", lengthOfLongestSubstring(s))
-    print("Expected: 1")
+    print("Output:", lengthOfLongestSubstringBF(s))
+    print("Expected: 1\n")
     
     s = "pwwkew"
     print("Example 3:")
     print("Input:", s)
-    print("Output:", lengthOfLongestSubstring(s))
-    print("Expected: 3")
+    print("Output:", lengthOfLongestSubstringBF(s))
+    print("Expected: 3\n")
     
